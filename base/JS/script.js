d4c65c9d97b9775df3ec05974689c501730ad0f5
Taras <luchakt2@gmail.com>
2023-12-09 17:29:44 +0200

    added the first task

--- base/JS/script.js
+++ base/JS/script.js
@@ -1,2 +1,27 @@
+let containerNull = null;
+
+function containerBoss(containerId) {
+	if (containerNull) {
+		containerNull.style.display = `none`;
+		document.body.style.backgroundImage = ``;
+	}
+
+	const container = document.getElementById(containerId);
+	container.style.display = (container.style.display === `grid`) ? `none` : `grid`;
+	containerNull = container;
+}
+
+function backgroundBodyColor(color) {
+	document.body.style.backgroundColor = color;
+	document.body.style.color = "white";
+	document.body.style.transition = "color 5s";
+}
+
+function backgroundBodyImage(image) {
+	document.body.style.backgroundImage = `url("${image}")`;
+	document.body.style.backgroundRepeat =  `no-repeat`;
+	document.body.style.color = "white";
+	document.body.style.transition = "color 5s";
+}
 
 
